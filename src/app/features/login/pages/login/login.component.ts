cfc39a3c08a6ebd215308f7347e89de873682578
Leslye <leslye.gomes@mjv.com.br>
2022-04-26 18:09:08 -0300

    mudanças no header footer e login student e student list

--- src/app/features/login/pages/login/login.component.ts
+++ src/app/features/login/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  modal!: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.modal = false;
   }
+
+  logar():void{
+    this.modal = true;
+  }
+  
+  fecharModal():void{
+    this.modal = false;
+  }
+  
+
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  name: any;
+  logado: any;
 
   ngOnInit(): void {
+    this.name = 'otavio';
+    this.logado = true;
   }
 
+  exit(): void{
+    this.router.navigate(['/login']);
+  }
+
+
+  navigateByUrl():void{
+    console.log("trocou de rota");
+  }
 }
